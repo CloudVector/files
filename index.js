c0ad29d5563bd2b2cf55c99c9506f7b166e6d8e1
Tamara Dukai <tamara@dukai.net>
2018-04-18 18:58:16 +0200

    Refactored to separate file the code

--- index.js
+++ index.js
@@ -1,40 +1,14 @@
 'use strict';
 
-const path = require('path');
-const fs = require('fs');
-
-const search = (startPath, options) => {
-    options = options || {};
-    options.files = options.files === undefined ? false : options.files;
-    return new Promise((resolve, reject) => {
-        fs.readdir(startPath, (err, data) => {
-            if (err) {
-                reject(err);
-            } else {
-                let result = data.filter((file) => {
-                    let fullPath = path.join(startPath, file);
-                    let isDir = fs.statSync(fullPath).isDirectory();
-                    let isExt = true;
-                    if (options.ext) {
-                        isExt = (path.extname(file).toLowerCase() === options.ext);
-                    }
-                    if (options.files) {
-                        return (!isDir && isExt);
-                    } else {
-                        return isDir;
-                    }
-                });
-                resolve(result);
-            }
-        });
-    });
-};
+const files = require('./lib/files.js');
 
 module.exports = {
+    // Gets the folder names for single level
     folders: async (startPath) => {
-        return await search(startPath, { files: false });
+        return await files(startPath, { files: false });
     },
+    // Gets the file names for single level or type if extension is specified
     names: async (startPath, extension) => {
-        return await search(startPath, { files: true, ext: extension });
+        return await files(startPath, { files: true, ext: extension });
     }
 };
